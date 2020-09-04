b459829334d62dac790269e9182a0467f144e056
MCR <der.marcreiterer@gmail.com>
2020-09-04 17:53:28 +0200

    There can now be multiple modifiers on an enemy

--- Tower/AsciiRogue/Assets/Scripts/EnemySpawner.cs
+++ Tower/AsciiRogue/Assets/Scripts/EnemySpawner.cs
@@ -80,7 +80,7 @@ public class EnemySpawner : MonoBehaviour
         so.dex = 10 + Mathf.FloorToInt(so.lvl * 0.5f);
         so.intell = 10 + Mathf.FloorToInt(so.lvl * 0.3f);
         so.end = 10 + Mathf.FloorToInt(so.lvl * 0.5f);
-
+        /*
         switch(so.enemySO.__modifier)
         {
             case EnemiesScriptableObject.E_modifier.strong:
@@ -125,9 +125,65 @@ public class EnemySpawner : MonoBehaviour
                 so.intell += Mathf.FloorToInt(so.intell * .5f);
                 so.end -= Mathf.FloorToInt(so.end * .1f);
                 break;
+        }*/
+
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.strong))
+        {
+            so.str += Mathf.FloorToInt(so.str * .75f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.nimble))
+        {
+            so.dex += Mathf.FloorToInt(so.dex * .75f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.smart))
+        {
+            so.intell += Mathf.FloorToInt(so.intell * .75f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.tough))
+        {
+            so.end += Mathf.FloorToInt(so.end * .75f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.weak))
+        {
+            so.str -= Mathf.FloorToInt(so.str * .5f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.clumsy))
+        {
+            so.dex -= Mathf.FloorToInt(so.dex * .5f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.stupid))
+        {
+            so.intell -= Mathf.FloorToInt(so.intell * .5f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.frail))
+        {
+            so.end -= Mathf.FloorToInt(so.end * .5f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.giant))
+        {
+            so.str += Mathf.FloorToInt(so.str * .4f);
+            so.dex += Mathf.FloorToInt(so.dex * .4f);
+            so.intell += Mathf.FloorToInt(so.intell * .4f);
+            so.end += Mathf.FloorToInt(so.end * .4f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.small))
+        {
+            so.str -= Mathf.FloorToInt(so.str * .2f);
+            so.dex -= Mathf.FloorToInt(so.dex * .2f);
+            so.intell -= Mathf.FloorToInt(so.intell * .2f);
+            so.end -= Mathf.FloorToInt(so.end * .2f);
+        }
+        if (so.enemySO.__modifier.HasFlag(EnemiesScriptableObject.E_modifier.caster))
+        {
+            so.str -= Mathf.FloorToInt(so.str * .1f);
+            so.dex -= Mathf.FloorToInt(so.dex * .1f);
+            so.intell += Mathf.FloorToInt(so.intell * .5f);
+            so.end -= Mathf.FloorToInt(so.end * .1f);
         }
 
-        switch(so.enemySO._Difficulty)
+
+
+        switch (so.enemySO._Difficulty)
         {
             case EnemiesScriptableObject.E_difficulty.easy:
                 so.str -= Mathf.FloorToInt(so.str * .2f);

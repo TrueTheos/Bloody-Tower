41eb1dd61a1f2c35a7a4ca8e1e53040d67c31f35
Loiuy123 <jeremi2811@wp.pl>
2020-09-12 13:54:12 +0200

    Tooltip polish and weight update

--- Tower/AsciiRogue/Assets/Scripts/MouseOver.cs
+++ Tower/AsciiRogue/Assets/Scripts/MouseOver.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    private TextMeshProUGUI textComponent;    
+
     public string displayedText; //text that will be displayed when mouse over it
     public Tooltip tooltip;
+
+    private void Start()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         tooltip.DisplayInfo(displayedText);
+        textComponent.fontStyle = FontStyles.Bold;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         tooltip.HideInfo();
+        textComponent.fontStyle = FontStyles.Normal;
     }
 }

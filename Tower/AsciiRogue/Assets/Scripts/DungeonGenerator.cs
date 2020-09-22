c420ad4f806aca26c497a925e011f434c1eb817c
MCR <der.marcreiterer@gmail.com>
2020-09-22 10:58:39 +0200

    changed mimic spawn rate

--- Tower/AsciiRogue/Assets/Scripts/DungeonGenerator.cs
+++ Tower/AsciiRogue/Assets/Scripts/DungeonGenerator.cs
@@ -371,7 +371,7 @@ public class DungeonGenerator : MonoBehaviour
                 }
                 else if(fixedLevel[inxedString] == "="[0])
                 {
-                    if(UnityEngine.Random.Range(0, 100) < 15)
+                    if(UnityEngine.Random.Range(0, 100) < 10)
                     {
                         MapManager.map[x, y] = new Tile();
                         MapManager.map[x, y].xPosition = x;

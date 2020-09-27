d09bb0e4a8e98af3d9fed2006989a58a0c0e2f16
Theos <jeremi2811@wp.pl>
2020-09-27 15:58:53 +0200

    Small fix

--- Tower/AsciiRogue/Assets/Scripts/AI/Basic/BasicTakeDamage.cs
+++ Tower/AsciiRogue/Assets/Scripts/AI/Basic/BasicTakeDamage.cs
@@ -13,7 +13,26 @@ public class BasicTakeDamage : BaseAIBehaviour<RoamingNPC>
     {
         ToWakeUp.Calculate(npc);
 
-        npc.__currentHp -= amount;
+        switch(dmgType)
+        {
+            case ItemScriptableObject.damageType.normal:
+                npc.__currentHp -= amount;
+                break;
+            case ItemScriptableObject.damageType.light:
+                if(npc.enemySO.W_light)
+                {
+                    npc.__currentHp -= Mathf.RoundToInt(amount * 1.5f);
+                }
+                else npc.__currentHp -= amount;
+                break;
+            case ItemScriptableObject.damageType.magic:
+                if (npc.enemySO.W_magic)
+                {
+                    npc.__currentHp -= Mathf.RoundToInt(amount * 1.5f);
+                }
+                else npc.__currentHp -= amount;
+                break;
+        } 
 
         npc.canvas.GetComponent<Animator>().SetTrigger("Shake");
         if (npc.__currentHp <= 0)
